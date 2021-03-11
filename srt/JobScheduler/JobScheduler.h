22f5579c06157095e6c2efd77cf50d91102a657c
Carl Pédimina <cpedimina@gmail.com>
2021-03-11 23:02:42 +0100

    - basic Semaphore implementation - cleaned JobScheluder dtor

--- srt/JobScheduler/JobScheduler.h
+++ srt/JobScheduler/JobScheduler.h
@@ -3,12 +3,31 @@
 
 #include "Base.h"
 #include <thread>
-#include <semaphore>
+#include <condition_variable>
 #include <mutex>
 #include <vector>
 
 namespace srt
 {
+	// ============================================================================
+	//
+	// Semaphore implementation. C++20 where are you?
+	//
+	// ============================================================================
+	class Semaphore
+	{
+	public:
+		Semaphore( int32_t count ) : m_count { count } { }
+
+		void Acquire( );
+		void Release( ); 
+
+	private:
+		std::mutex					m_mutex;
+		std::condition_variable		m_cv;
+		int32_t						m_count;
+	};
+	
 	// ============================================================================
 	//
 	//
@@ -34,12 +53,20 @@ namespace srt
 		JobScheduler( const size_t threadCount );
 		~JobScheduler();
 
+		void PushJob();
+
 	private:
 		static void ThreadFunc( JobScheduler * js );
 	
 		using ThreadContainer = std::vector< std::thread * >;
 		ThreadContainer	m_threads;
 		size_t			m_threadCount;
+
+		Semaphore		m_sem{ 0 };
+
+
+		volatile bool	m_exit { false };
+
 	
 	};
 
